e4c18f4bc6cc751d903f08dabb60c7e8ca010ed7
Gabriel Santana <gabriel.santana@redbelt.com.br>
2023-03-31 17:06:27 -0300

    Estrutura do projeto pronta

---
+++ Gallery_Project/resources/views/index.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Adicionando o CSS do Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Galeria de Fotos</title>
+</head>
+<body>
+    
+    <div class="container">
+        <h1 class="text-center mt-5">Galeria de Fotos</h1>
+        <div class="row mt-5">
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="Foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Foto 1</h5>
+                        <p class="card-text">Descrição da Foto 1.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="Foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Foto 2</h5>
+                        <p class="card-text">Descrição da Foto 2.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="Foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Foto 3</h5>
+                        <p class="card-text">Descrição da Foto 3.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="Foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Foto 4</h5>
+                        <p class="card-text">Descrição da Foto 4.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="Foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Foto 5</h5>
+                        <p class="card-text">Descrição da Foto 5.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img src="https://via.placeholder.com/350x150" class="card-img-top" alt="Foto">
+                    <div class="card-body">
+                        <h5 class="card-title">Foto 6</h5>
+                        <p class="card-text">Descrição da Foto 6.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+
+    
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
--- Gallery_Project/routes/web.php
+++ Gallery_Project/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PhotoController::class, 'index']);
+
+
